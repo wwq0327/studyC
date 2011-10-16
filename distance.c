d6868e8cc100ce2f3c6910f462bc30e9fbe02615
wwq0327 <wwq0327@gmail.com>
2011-10-16 11:03:29 +0800

    c

---
+++ distance.c
@@ -0,0 +1,22 @@
+#include <math.h>
+#include <stdio.h>
+
+double distance(double x1, double x2, double y1, double y2);
+
+int main(void)
+{
+  printf("distance is %f\n", distance(1.0, 2.0, 4.0, 6.0));
+  return 0;
+}
+
+double distance(double x1, double y1, double x2, double y2)
+{
+  double dx = x2 - x1;
+  double dy = y2 - y1;
+  /* printf("dx is %f\ndy is %f\n", dx, dy); */
+  double dsquared = dx * dx + dy * dy;
+  /* printf("dsquared is %f\n", squared); */
+  double result = sqrt(dsquared);
+
+  return result;
+}
