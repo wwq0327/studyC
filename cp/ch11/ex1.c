6227b44ab231164d0f077edcc25d52779a81c34e
wwq0327 <wwq0327@gmail.com>
2011-12-01 11:56:53 +0800

    ch11

---
+++ cp/ch11/ex1.c
@@ -0,0 +1,23 @@
+/* ex1.c
+ *
+ * date: 2011-12-01
+ * author: wwq0327 <wwq0327@gmail.com>
+ */ 
+
+#include <stdio.h>
+
+int main()
+{
+
+	return 0;
+}
+
+char *read(char *string, int n)
+{
+
+	while (*string)
+	{
+		puts(string);
+	}
+	returen string;
+}
